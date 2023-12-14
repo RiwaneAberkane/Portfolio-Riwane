562032985dd56ac2bc9861c210cb429ef5459743
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2023-12-14 02:42:53 +0100

    wip modal

---
+++ src/data/projectsData.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    idmodal: "modal1-trigger",
+    image: "/biomarket.png",
+    title: "Biomarket",
+    body: "Conception d'une application de gestion de stock pour une entreprise fictive en utilisant la stack MERN afin de valider mon titre de développeur web et web mobile.",
+    titlemodal: "Biomarket - Gestion Écoresponsable",
+    article:
+      "Découvrez Biomarket, une entreprise fictive créée pour valider ma formation en développement web et mobile. Guidé par des principes écoresponsables, ce projet utilise la stack MERN pour une base technique solide. Avec des user stories axées sur l'éco-responsabilité, une gestion éthique des utilisateurs via JWT, et une interface réactive en React, Biomarket représente une vision inspirante de la gestion commerciale du futur. Explorez les améliorations futures pour une expérience utilisateur de qualité et une entreprise toujours plus écoresponsable.",
+    stackmodal: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg",
+    ],
+    github: "https://github.com/RiwaneAberkane/Biomarket_MERN",
+  },
+  {
+    idmodal: "modal2-trigger",
+    image: "/restaurantApp.png",
+    title: "Restaurant App",
+    body: "Création d'une application React qui utilise les hooks afin d'enregistrer les restaurants préférés des utilisateurs via le stockage local.",
+    stackmodal: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    ],
+    github: "https://github.com/RiwaneAberkane/restaurant-app",
+  },
+  {
+    idmodal: "modal3-trigger",
+    image: "/selfCare.png",
+    title: "CitatioNinja",
+    body: "Développement d'une petite application en JavaScript qui génère des citations inspirantes pour égayer votre journée.",
+    stackmodal: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+    ],
+    github: "https://github.com/RiwaneAberkane/self-care-center",
+  },
+  {
+    idmodal: "modal4-trigger",
+    image: "/maquetteSimplon.png",
+    title: "Maquette Simplon",
+    body: "Création d'un site web avec une version responsive en utilisant HTML et CSS, en m'inspirant d'une maquette Figma pour assurer une conception harmonieuse sur différentes tailles d'écrans.",
+    stackmodal: [
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg",
+      "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+    ],
+    github: "https://github.com/RiwaneAberkane/MaquetteSimplon",
+  },
+];
