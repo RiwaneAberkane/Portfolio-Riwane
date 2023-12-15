72c94e485f7cf8b723b72f62c576a0695af05b7a
RiwaneAberkane <riwane.aberkane.pro@outlook.fr>
2023-12-15 17:27:47 +0100

    clean blog post

---
+++ src/data/blogData.js
@@ -0,0 +1,9 @@
+export const blogs = [
+  {
+    idmodal: "1",
+    image: "/simplon.png",
+    title: "Simplon",
+    body: "Mon expérience en tant qu'apprenti chez Simplon... (À venir)",
+    link: "https://medium.com/",
+  },
+];
